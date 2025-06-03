5e3c51d6be031eb90738ec251c77d36d9d8c3c37
Moe <mchehab20038@gmail.com>
2025-06-02 17:48:52 -0700

    changes

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -19,30 +19,32 @@ const ContactPage = () => {
     <div className="flex flex-col min-h-screen text-white font-sans">
       <Toaster />
       <Header />
-      <main className="container px:4 md:px-12 w-[30rem] mx-auto pb-12">
-        <h1 className="text-4xl font-extrabold tracking-tight text-black sm:text-5xl dark:text-white">Contact Us</h1>
-        <form action={action} className="w-full mt-6 space-y-4">
+      <main className="min-h-screen mx-auto w-[350px] items-center justify-center py-0">
+        <div className="w-full max-w-md bg-transparent p-0 rounded-lg">
+          <h1 className="text-4xl font-extrabold tracking-tight text-black sm:text-5xl dark:text-white text-center mb-6">Contact Us</h1>
+          <form action={action} className="w-full space-y-4">
             <div>
             <label className="block text-sm font-medium text-gray-700 dark:text-white">
                 Name
             </label>
-            <input name="name" type="text" className="dark:text-white text-black w-full mt-1 p-2 border border-gray-300 rounded-md" />
+            <input name="name" type="text" className="dark:text-white text-black w-full mt-1 p-2 border border-gray-300 rounded-md" required />
             </div>
            
             <div>
             <label className="block text-sm font-medium text-gray-700 dark:text-white">
                 Email
             </label>
-            <input name="email" type="email" className="dark:text-white text-black w-full mt-1 p-2 border border-gray-300 rounded-md" />
+            <input name="email" type="email" className="dark:text-white text-black w-full mt-1 p-2 border border-gray-300 rounded-md" required />
             </div>
             <div>
             <label className="block text-sm font-medium text-gray-700 dark:text-white">
                 Message
             </label>
-            <textarea name="message" className="dark:text-white h-[10rem] text-black w-full mt-1 p-2 border border-gray-300 rounded-md"></textarea>
+            <textarea name="message" className="dark:text-white h-[10rem] text-black w-full mt-1 p-2 border border-gray-300 rounded-md" required></textarea>
             </div>
             <button type="submit" className="cursor-pointer w-full mt-6 bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700">Send</button>
         </form>
+        </div>
       </main>
       <Footer />
       </div>
